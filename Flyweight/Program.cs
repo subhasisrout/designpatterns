6a82c596cdfce29e5bf92417148ee34ac1af5019
Subhasis Rout <surout@microsoft.com>
2024-09-03 01:57:01 +0530

    checkin

---
+++ Flyweight/Program.cs
@@ -0,0 +1,13 @@
+﻿// The Flyweight pattern is a structural design pattern that minimizes memory usage or computational expenses by sharing as much as possible with related objects. It is used to reduce the number of objects created and to decrease memory footprint and increase performance.
+// Check sudoCODE youtube channel, Excellent Explaination - https://www.youtube.com/watch?v=8cL9KbHS5kE&list=PLTCrU9sGybupCpY20eked6blbHI4zZ55k&index=19
+// #TODO
+namespace Flyweight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
